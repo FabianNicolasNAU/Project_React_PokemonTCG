9be7da9a36e6bd26ef2e831950d7a612519f8652
AzocarEnrique <azocarinost@gmail.com>
2022-12-15 23:57:24 -0300

    sas

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,7 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+//import { useNavigate } from "react-router-dom";
 import '../App.css';
+
+
+  
+//const history = useNavigate();
+  
+
 
 function Login() {
   return (
@@ -26,6 +34,18 @@ function Login() {
           </Form.Label>
           <Col column lg={5}>
             <Form.Control type="text" placeholder="Password" />
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col>
+          </Col>
+          <Col>
+            <Button color="primary" className="px-4">
+              Login
+            </Button>
+          </Col>
+          <Col>
           </Col>
         </Row>
       </Form>
