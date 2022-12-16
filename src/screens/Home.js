d9329b71447ea03455c444f8af7c2a6fb489d1be
FabianNicolasNAU <fabian-almonacid@hotmail.com>
2022-12-15 23:04:25 -0300

    Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -25,34 +25,57 @@ function Home() {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
   };
 
   return (
-    <div className="container">
-      <img class="tcgicon" src={logo}/>
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
+    <div className="bkgd">
+      <div className="container">
+        <img className="tcgicon" src={logo}/>
+        <div className="container-carousel">
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+          </Slider>
         </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
+      </div>
     </div>
+
   );
 }
 export default Home
