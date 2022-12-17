38f51cfd4c302fb939285483222aecbbd15361fc
FabianNicolasNAU <fabian-almonacid@hotmail.com>
2022-12-17 12:37:44 -0300

    cambios de estilos

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -34,10 +34,12 @@ function Home() {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 800,
     centerMode: true,
-    centerPadding: '60px',
+    centerPadding: '80px',
     slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
     responsive: [
     {
       breakpoint: 768,
@@ -45,7 +47,7 @@ function Home() {
         arrows: false,
         centerMode: true,
         centerPadding: '40px',
-        slidesToShow: 3
+        slidesToShow: 2
       }
     },
     {
@@ -85,7 +87,7 @@ function Home() {
             <Slider {...settings}>
               {alto && alto.map((item) => (
                 <div className="card-position">
-                  <img className="card-image" src={item.images.small}></img>
+                  <img className="card-image" src={item.images.small} alt="Pokemon Card"></img>
                 </div>
               ))}
             </Slider>
