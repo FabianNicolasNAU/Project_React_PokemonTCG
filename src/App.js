58d43db47df0910a3ba4c6432a519aa2e04c81d7
FabianNicolasNAU <fabian-almonacid@hotmail.com>
2022-12-18 00:02:45 -0300

    xds

--- src/App.js
+++ src/App.js
@@ -7,15 +7,6 @@ import Card from './screens/Card';
 const token  =  "5d2272df-24bd-4cdc-a681-4c8ae7722c85";
 
 function App() {
-<<<<<<< HEAD
-  let auth = false
-  if(localStorage.auth) auth = true
-  else{
-    auth = false
-    
-  } 
-=======
->>>>>>> 93a0756854bf758155ac7d20424a2991f1a78e98
   return (
     <HashRouter>
       <Route exact path="/" component={() => <Home autorizado={auth}/>}/>
