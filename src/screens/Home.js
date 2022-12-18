05df79fb2642025432145000d47206cc2958e950
AzocarEnrique <azocarinost@gmail.com>
2022-12-18 16:29:15 -0300

    validacion

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -134,8 +134,9 @@ function Home() {
                 </tbody>
               </Table>
             </div>
-          </div>
+          </div>   
           <div className="container-carousel" style={{zIndex:'-1'}}>
+          <h3 className="text-center" style={{paddingBottom:'1vh'}}>Cartas primera edición más costosas</h3> 
             <Slider {...settings}>
               {alto && alto.map((item) => (
                 <div className="card-position">
@@ -145,7 +146,7 @@ function Home() {
             </Slider>
           </div>
           <div>
-            <h3>Cartas mas Valiosas:</h3>    
+            <h3 className="text-center" style={{paddingBottom:'1vh'}}>Mix de cartas más valiosas</h3>    
             <Lista {...post}/> 
           </div>           
       </div>
